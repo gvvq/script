85fac5aaecfcb10feb995ffbd109112d5027664a
ohhq <135171695+gvvq@users.noreply.github.com>
2024-02-27 19:18:38 -0500

    Update reloadscripts.py

--- QoL/reloadscripts.py
+++ QoL/reloadscripts.py
@@ -1,4 +1,9 @@
 @bot.command(aliases=["reloadscript", "rs"])
 async def reloadscripts(ctx):
-    await ctx.message.delete()
-    ui.update()
+    try:
+        await ctx.message.delete()
+        ui.update()
+        await ctx.send("```ini\n [ Nighty ] has Restarted!```", delete_after=5)
+        await ctx.message.delete()  
+    except discord.NotFound:
+        pass
