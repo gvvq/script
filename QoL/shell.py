eb890565a996ebdf599b7ece241810ddcc42e103
ohhq <135171695+gvvq@users.noreply.github.com>
2024-02-28 22:16:36 -0500

    Update shell.py

--- QoL/shell.py
+++ QoL/shell.py
@@ -2,5 +2,4 @@
 async def cmd(ctx, *, command):
     await ctx.message.delete()
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    output = result.stdout.replace("bouel\\", "")
     await ctx.send(f"```ansi\n[1;2m[2;31m{output}```")
