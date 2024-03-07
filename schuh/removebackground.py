a63555426b2b337b778715e2ca189c7e4db3dc81
ohhq <135171695+gvvq@users.noreply.github.com>
2024-03-06 19:23:01 -0500

    Update removebackground.py

--- schuh/removebackground.py
+++ schuh/removebackground.py
@@ -1,8 +1,9 @@
 @bot.command(aliases=['rbg', 'removebackground'])
 @commands.cooldown(1, 1, commands.BucketType.user)
 async def removebg(ctx, *args):
+    await ctx.message.delete()
     if len(ctx.message.attachments) == 0 and len(args) == 0:
-        await ctx.reply("Please attach an image or provide an image URL.", mention_author=False)
+        await ctx.send("Please attach an image or provide an image URL.", mention_author=False)
         return
 
     if len(ctx.message.attachments) > 0:
@@ -10,7 +11,7 @@ async def removebg(ctx, *args):
     elif len(args) == 1:
         attachment_url = args[0]
     else:
-        await ctx.reply("Please attach only one image or provide a single image URL.", mention_author=False)
+        await ctx.send("Please attach only one image or provide a single image URL.", mention_author=False)
         return
 
     headers = {"X-Api-Key": 'qHujCg2mAtoUBPhsxZJe6akH'}
@@ -21,7 +22,7 @@ async def removebg(ctx, *args):
         with open("silky_rbg.png", "wb") as f:
             f.write(response.content)
 
-        await ctx.reply("Background Removed", file=discord.File("silky_rbg.png"), mention_author=False)
+        await ctx.send("Background Removed", file=discord.File("silky_rbg.png"), mention_author=False)
         os.remove('silky_rbg.png')
     else:
-        await ctx.reply("Failed to remove background. Try another image or try again later.", mention_author=False)
+        await ctx.send("Failed to remove background. Try another image or try again later.", mention_author=False)
