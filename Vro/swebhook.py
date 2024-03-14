1741605ae31f9aacfca1c2b0c74ff6337ca179e0
ohhq <135171695+gvvq@users.noreply.github.com>
2024-03-14 18:56:50 -0400

    Update swebhook.py

--- Vro/swebhook.py
+++ Vro/swebhook.py
@@ -1,6 +1,6 @@
 @bot.command()
 async def swebhook(ctx, webhook_url: str, *, message: str):
+    await ctx.message.delete()
     data = {'content': message}
     requests.post(webhook_url, json=data)
     await asyncio.sleep(1)
-    await ctx.message.delete()
