c80e3ec7dbe8a4e25d4a293d9197d87b53e4a83c
ohhq <135171695+gvvq@users.noreply.github.com>
2024-03-24 18:39:41 -0400

    Update hex.py

--- QoL/hex.py
+++ QoL/hex.py
@@ -4,10 +4,6 @@ async def hex(ctx, color: str):
     api_url = f"https://api.popcat.xyz/color/{color}"
     response = requests.get(api_url)
     data = response.json()
-
-    hex_code = data.get("hex", "Unknown")
-    rgb_code = data.get("rgb", "Unknown")
     image_url = data.get("color_image", "")
-
-    await ctx.send(f"```ini\n[ {hex_code} ]\n[ {rgb_code} ]```")
+    
     await ctx.send(image_url)
